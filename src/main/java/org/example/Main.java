031852effa36c8ba495bdc0e5f6796566313e61c
nzr34 <nezirgokce@gmail.com>
2023-05-30 21:19:15 -0400

    testjenkins

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello world!");
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Hello world 2!");
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
